37af1aeba86c2a10097ec84f78dd5f3f34522a96
Jordi Carbonell <jordicarbonell@jordi.local>
2025-07-12 14:49:26 +0700

    canvi dashboard negocis

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,45 +2,66 @@
 
 import { Responsive, WidthProvider } from "react-grid-layout";
 import Widget from "@/components/Widget";
+import CalculadoraPladur from "@/components/CalculadoraPladur";
+import CalculadoraMetresQuadrats from "@/components/CalculadoraMetresQuadrats";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
-import CalculadoraIVA from "@/components/CalculadoraIVA";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function Dashboard() {
   const layout = [
-    { i: "1", x: 0, y: 0, w: 1, h: 1, static: true }, // 1x1
-    { i: "2", x: 1, y: 0, w: 1, h: 2, static: true }, // 1x2
-    { i: "3", x: 2, y: 0, w: 2, h: 2, static: true }, // 2x2
-    { i: "4", x: 0, y: 1, w: 1, h: 1, static: true }, // 1x1
+    { i: "1", x: 0, y: 0, w: 1, h: 1, static: true },
+    { i: "3", x: 0, y: 1, w: 1, h: 1, static: true },
+    { i: "4", x: 1, y: 0, w: 1, h: 2, static: true },
+    { i: "5", x: 2, y: 0, w: 2, h: 2, static: true },
+    { i: "6", x: 0, y: 2, w: 2, h: 2, static: true },
+    { i: "7", x: 2, y: 2, w: 1, h: 2, static: true },
+    { i: "8", x: 3, y: 2, w: 1, h: 1, static: true },
+    { i: "9", x: 3, y: 3, w: 1, h: 1, static: true },
   ];
 
   return (
-    <div className="p-6 min-h-screen bg-[#525B6B]">
-      <h1 className="text-3xl font-bold text-white text-center mb-8">Calculadores</h1>
+    <main className="min-h-screen bg-[#525B6B] p-6">
+      <h1 className="text-3xl font-bold text-white text-center mb-8">Negocis</h1>
 
       <ResponsiveGridLayout
         className="layout"
         layouts={{ lg: layout }}
         breakpoints={{ lg: 1200, md: 996, sm: 768 }}
         cols={{ lg: 6, md: 4, sm: 2 }}
-        rowHeight={200}
-        margin={[20, 20]} // separació horitzontal i vertical
+        rowHeight={240}
+        margin={[20, 20]}
         isResizable={false}
         isDraggable={false}
       >
         <div key="1">
-          <Widget size="1x1" title="IVA" /> 
-        </div>
-        <div key="2">
-          <Widget size="1x2" title="Descompte" />
+          <Widget size="1x1" title="Calculador IVA">
+            <CalculadoraMetresQuadrats />
+          </Widget>
         </div>
         <div key="3">
-          <Widget size="2x2" title="Beneficis" />
+          <Widget size="1x1" title="2">...</Widget>
         </div>
         <div key="4">
-          <Widget size="1x1" title="Nou Widget" />
+          <Widget size="1x2" title="Calculadora Pladur">
+            <CalculadoraPladur />
+          </Widget>
+        </div>
+        <div key="5">
+          <Widget size="2x2" title="Totxanes">...</Widget>
+        </div>
+        <div key="6">
+          <Widget size="2x2" title="Totxanes">...</Widget>
+        </div>
+        <div key="7">
+          <Widget size="1x2" title="Totxanes">...</Widget>
+        </div>
+        <div key="8">
+          <Widget size="1x1" title="Grava2">...</Widget>
+        </div>
+        <div key="9">
+          <Widget size="1x1" title="Grava2">...</Widget>
         </div>
       </ResponsiveGridLayout>
 
@@ -52,7 +73,6 @@ export default function Dashboard() {
           background-size: 200px 200px;
         }
       `}</style>
-    </div>
+    </main>
   );
 }
-
